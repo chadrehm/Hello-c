a5d00f0dfb9d6e5269a1c7089ac8b6b0de87cc77
chad rehm <chad.rehm@kineticdata.com>
2022-09-23 18:09:56 -0500

    added dynamic name

--- Hello.c
+++ Hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("Hello World\n");
+	printf("Hello World.\n");
+	char name[] = "Chad";
+	printf("This is %s speaking.\n", name);
 	
 	return 0;
 }
